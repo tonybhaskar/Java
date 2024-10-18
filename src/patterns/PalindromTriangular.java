40a964b93e8fb7068cee195e523d1bd64d1c22c0
TonyBhaskar <tonybhaskar23@gmail.com>
2024-10-18 21:58:14 +0530

    commit

--- src/patterns/PalindromTriangular.java
+++ src/patterns/PalindromTriangular.java
@@ -1,10 +1,37 @@
 package patterns;
 
+/*
+ *       1
+ *     2 1 2
+ *   3 2 1 2 3 
+ * 4 3 2 1 2 3 4
+ */
+
 public class PalindromTriangular {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		int n= 4;
+		for(int i = 1; i<=n; i++) {
+			//leading zeros
+			for(int j = n-i; j>=1; j--) {
+				System.out.print("  ");
+			}
+			
+			//i to 1
+			for(int j = i; j>=1; j--) {
+				System.out.print(j+" ");
+			}
+			
+			//2 to i
+            for (int j = 2; j <= i; j++) {
+                System.out.print(j );
+                if(j<i)
+                	System.out.print(" ");
+            }
+            
+			System.out.println();
+		}
+    }
+	
 
 }
